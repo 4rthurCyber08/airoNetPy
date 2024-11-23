c00992694c3a0f445ea69f67d92a118f2386121a
ArJames Inson <aerin.james820@gmail.com>
2024-11-23 14:51:30 +0800

    Update autowifi.py

--- airoNetPy/autowifi.py
+++ airoNetPy/autowifi.py
@@ -13,17 +13,24 @@ apConfig = deviceData['aironetConfig']
 deviceConfig = [
     f'hostname {apConfig["hostname"]}',
     f'dot11 ssid {apConfig["ssid"]}',
+    f'vlan {apConfig["vlan"]}',
     f'authentication {apConfig["authentication"]}',
     f'authentication key-management {apConfig["key-man"]}',
     f'wpa-psk ascii {apConfig["wifi-pass"]}',
     'guest-mode',
-    'Default Int Dot11Radio 0',
-    'Int Dot11Radio 0',
-    f'channel {apConfig["channel"]}',
+    'default Int Dot11Radio 0',
+    'default interface gigabitEthernet 0',
+    'int dot11radio 0',
     'no shut',
+    f'channel {apConfig["channel"]}',
     f'encryption mode ciphers {apConfig["encr-mod"]}',
+    f'encryption vlan {apConfig["vlan"]} mode ciphers {apConfig["encr-mod"]}',
     f'ssid {apConfig["ssid"]}',
     'exit',
+    f'interface dot11radio 0.{apConfig["vlan"]}',
+    f'encapsulation dot1q {apConfig["vlan"]} native',
+    'bridge-group 1',
+    'exit'
 ]
 
 # connect to the device
